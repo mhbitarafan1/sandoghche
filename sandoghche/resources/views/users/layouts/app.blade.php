e5e7033fedffed77b566b3a75599cc2bce477150
mhbitarafan1 <mhb.qom@gmail.com>
2023-06-07 02:19:32 +0430

    feature: add piwik JavaScript Tracking Code to project

--- sandoghche/resources/views/users/layouts/app.blade.php
+++ sandoghche/resources/views/users/layouts/app.blade.php
@@ -1,6 +1,23 @@
 <!DOCTYPE html>
 <html lang="fa" dir="rtl" style="font-size: 100%;">
 <head>
+
+    <!-- Matomo Piwik -->
+    <script>
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//piwik.sandoghche.mhbitarafan.ir/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
+    <!-- End Matomo Piwik Code -->
+
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
