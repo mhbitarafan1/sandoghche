7ba040a9c46cb3b840a027814cf87aefe8a91e7f
mhbitarafan1 <mhb.qom@gmail.com>
2023-07-03 20:01:07 +0430

    hotfix: add maintenance page to blades

---
+++ sandoghche/resources/views/errors/503.blade.php
@@ -0,0 +1,13 @@
+@extends('errors.layouts.master')
+
+@section('title')
+    صندوقچه | خطای 500
+@endsection
+@section('content')
+    <br><br>
+    <h2>صندوقچه در حال تعمیرات و به روز رسانی می باشد</h2>
+    <br>
+    <h3>لطفا بعدا مراجعه نمایید<br> از شکیباییی شما سپاس گذاریم</h3>
+
+@endsection
+
